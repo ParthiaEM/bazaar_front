d5298005799770f11635dfff4b11c469c45c6187
minseok0415 <kmskorea14@gmail.com>
2023-11-06 18:04:25 +0900

    추가 커밋

--- bazzar/src/components/Login.jsx
+++ bazzar/src/components/Login.jsx
@@ -5,15 +5,13 @@ import Hide from '../images/password/hide.svg';
 import Close from '../images/close.svg';
 import { customAxios } from '../customAxios';
 
-export default function Registration(props) {
+export default function Login(props) {
     const [isHide, setIsHide] = useState(false)
     const [id, setId] = useState("")
     const [password, setPassword] = useState("")
-    const [account, setAccount] = useState("")
-    const [bank, setBank] = useState("none")
     const [alert, setAlert] = useState(0)
 
-    function registration(e) {
+    function login(e) {
         e.preventDefault()
 
         if (id.length === 0) {
@@ -22,43 +20,34 @@ export default function Registration(props) {
         } if (password.length === 0) {
             setAlert(2)
             return
-        } if (account.length === 0) {
-            setAlert(3)
-            return
-        } if (bank === 'none') {
-            setAlert(4)
-            return
         }
 
         const userInfo = {
             "userId" : id,
             "userPassword" : password,
-            "userAccount" : bank+' '+account,
-            "lux" : 250,
         }
+        console.log(userInfo)
         postUserInfo(userInfo)
     }
 
     async function postUserInfo(userInfo) {
         await customAxios
-            .post("/user/register",
+            .post("/user/login",
                 new URLSearchParams(userInfo)
             )
             .then(function (response) {
+                if (response.data.login === 'failed') {
+                    setAlert(3)
+                    return
+                }
                 console.log(response)
                 setId("")
                 setPassword("")
-                setAccount("")
-                setBank("none")
                 setAlert(0)
                 props.close(false)
             })
             .catch(function (error) {
                 console.log(error)
-                if (error.response.request.status === 409) {
-                    setId("")
-                    setAlert(5)
-                }
             })
     }
 
@@ -68,22 +57,21 @@ export default function Registration(props) {
     }
 
     return (
-        <SRegist>
+        <SLogin>
             <Empty />
             <SBox>
                 <BoxHeader>
                     회원 가입 / 로그인
                 </BoxHeader>
                 <Switch>
-                    <Selected>가입</Selected>
-                    <Unselected onClick={() => change()}>로그인</Unselected>
+                    <Unselected onClick={() => change()}>가입</Unselected>
+                    <Selected>로그인</Selected>
                 </Switch>
                 <SForm>
                     <Form>
                         <Legend>아이디</Legend>
                         <Input value={id} placeholder='아이디' onChange={e => setId(e.target.value)} />
                         {alert === 1 && <Alert>아이디를 작성해주세요.</Alert>}
-                        {alert === 5 && <Alert>중복된 아이디입니다.</Alert>}
                     </Form>
                     <Form>
                         <Legend>패스워드</Legend>
@@ -101,23 +89,12 @@ export default function Registration(props) {
                         </Wrap>
                         {alert === 2 && <Alert>패스워드를 작성해주세요.</Alert>}
                     </Form>
-                    <Form>
-                        <Legend>계좌번호</Legend>
-                        <Input value={account} placeholder='계좌번호' onChange={e => setAccount(e.target.value)} />
-                        <ToRight>
-                            <SelectBank value={bank} onChange={e => setBank(e.target.value)}>
-                                <option value='none'>은행명</option>
-                                <option value='카카오뱅크'>카카오뱅크</option>
-                            </SelectBank>
-                            {alert === 4 && <Alert1>은행을 선택해주세요</Alert1>}
-                        </ToRight>
-                        {alert === 3 && <Alert>계좌번호를 작성해주세요.</Alert>}
-                    </Form>
                     <Submit
                         type='submit'
-                        onClick={e => registration(e)}
-                    >가입</Submit>
+                        onClick={e => login(e)}
+                    >로그인</Submit>
                 </SForm>
+                {alert === 3 && <Alert1>로그인에 실패했습니다.</Alert1>}
             </SBox>
             <Empty>
                 <CloseButton
@@ -125,11 +102,11 @@ export default function Registration(props) {
                     onClick={() => props.close(false)}
                 />
             </Empty>
-        </SRegist>
+        </SLogin>
     )
 }
 
-const SRegist = styled.div`
+const SLogin = styled.div`
     width: 100vw;
     height: 100vh;
     top: 0;
@@ -195,8 +172,8 @@ const SForm = styled.form`
     flex-direction: column;
     width: 60%;
     height: 50%;
-    margin: 28px auto;
-    gap: 32px;
+    margin: 60px auto;
+    gap: 80px;
 `;
 
 const Form = styled.div`
@@ -226,21 +203,6 @@ const HideButton = styled.img`
     position: absolute;
     right: 12px;
     top: 14px;
-    cursor: pointer;
-`;
-
-const ToRight = styled.div`
-    display: flex;
-    justify-content: right;
-`;
-
-const SelectBank = styled.select`
-    width: 40%;
-    height: 40px;
-    padding: 4px;
-    margin-right: 0;
-    font-size: 15px;
-    border: 1px solid #9a9a9a;
     cursor: pointer;
 `;
 
@@ -278,9 +240,9 @@ const Alert = styled.div`
 `;
 
 const Alert1 = styled.div`
-    position: absolute;
-    width: 200px;
-    text-align: left;
+    position: relative;
+    width: 100%;
+    text-align: center;
     color: red;
     font-size: 12px;
     font-weight: bold;
