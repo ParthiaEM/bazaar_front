7c29c414f04c4db39512e9246998ce01f93f5a32
minseok0415 <kmskorea14@gmail.com>
2023-11-13 15:54:58 +0900

    저장된 쿠키로 유저 정보 불러오기

--- bazzar/src/App.js
+++ bazzar/src/App.js
@@ -3,14 +3,29 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Main from './components/Main.jsx';
 import { getCookie } from './cookies.js';
+import { customAxios } from './customAxios.js';
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userInfo, setUserInfo] = useState({})
 
+  async function getUser(token) {
+    await customAxios
+        .get('/user?token='+token)
+        .then(function (response) {
+            setUserInfo(response.data)
+        })
+        .catch(function (error) {
+            console.log(error)
+        })
+}
+
   useEffect(() => {
     const token = getCookie('token')
-    if (token !== undefined) setIsLoggedIn(true)
+    if (token !== undefined) {
+      setIsLoggedIn(true)
+      getUser(token)
+    }
     else setIsLoggedIn(false)
   }, [])
 
