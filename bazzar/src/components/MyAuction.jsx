0ee44c75733a503a9d011e20e4b918646277fd2b
minseok0415 <kmskorea14@gmail.com>
2023-11-20 20:22:57 +0900

    아이디어 공개하기 퍼블리싱 완료

--- bazzar/src/components/MyAuction.jsx
+++ bazzar/src/components/MyAuction.jsx
@@ -16,6 +16,7 @@ import CoinSVG from '../images/coin.svg'
 import StopAuction from "./StopAuction"
 import { getCookie } from "../cookies"
 import SuccessAuction from "./SuccessAuction"
+import OpenIdea from "./OpenIdea"
 
 export default function MyAuction() {
     const {id} = useParams()
@@ -134,7 +135,9 @@ export default function MyAuction() {
                             <Field src={toIcon(ideaData.ideaField)} />
                         </Line>
                         <Title>{ideaData.ideaName}</Title>
-                        {retouch ?
+                        {ideaData.purchasedUserId !== 0 ?
+                            <Empty1 /> :
+                        retouch ?
                             <Line1>
                                 <Span1
                                     onClick={() => setRetouch(false)}
@@ -165,13 +168,19 @@ export default function MyAuction() {
                                 )}
                             </Coins>
                             <Span>{toPrice(ideaData.price)} ₩</Span>
-                            {ideaData.count !== 0 ? <Button onClick={() => setShowModal(2)}>낙찰하기</Button> : <Button1>입찰을 기다려주세요</Button1>}
+                            {ideaData.count !== 0 ?
+                            ideaData.purchasedUserId !== 0 ?
+                                <Button onClick={() => setShowModal(3)}>공개하기</Button> :
+                                <Button onClick={() => setShowModal(2)}>낙찰하기</Button> :
+                                <Button1>입찰을 기다려주세요</Button1>
+                            }
                         </Bid>
                     </Side1>
                 </Wrap>
             </Column>
             {showModal === 1 && <StopAuction close={setShowModal} id={id} />}
             {showModal === 2 && <SuccessAuction close={setShowModal} id={id} bidder={lastUser.userId} price={ideaData.price} />}
+            {showModal === 3 && <OpenIdea close={setShowModal} />}
         </SAuction>
     )
 }
@@ -271,6 +280,10 @@ const Span1 = styled.span`
     cursor: pointer;
 `
 
+const Empty1 = styled.div`
+    height: 20px;
+`
+
 const Textarea = styled.textarea`
     width: calc(100% - 24px);
     height: 396px;
@@ -315,7 +328,7 @@ const Coin = styled.img`
 
 const Button = styled.div`
     width: 50%;
-    background-color: #FFEEE8;
+    background-color: #FDF7F5;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -327,7 +340,6 @@ const Button = styled.div`
 
 const Button1 = styled(Button)`
     width: 70%;
-    background-color: #FDF7F5;
     color: #9A9A9A;
     cursor: default;
     word-break: keep-all;
---
+++ bazzar/src/components/OpenIdea.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import styled from "styled-components"
+import Complete from "./Complete";
+
+export default function OpenIdea({close}) {
+    const [show, setShow] = useState(false)
+
+    async function open() {
+        setShow(true)
+    }
+
+    return (
+        <SGetStarted>
+            <SBox>
+                <BoxHeader>낙찰자에게 아이디어를 공개할까요?</BoxHeader>
+                <Wrap>
+                    <Button onClick={() => close(0)}>아니오</Button>
+                    <Button1 onClick={() => open()}>네</Button1>
+                </Wrap>
+                <Span>낙찰자의 결제를 확인한 후 진행해주세요</Span>
+            </SBox>
+            {show && <Complete say="아이디어를 공개했습니다!" />}
+        </SGetStarted>
+    )
+}
+
+const SGetStarted = styled.div`
+    width: 100vw;
+    max-height: max-content;
+    min-height: 100vh;
+    top: 0;
+    left: 0;
+    position: fixed;
+    background-color: #00000030;
+    display: flex;
+    justify-content: center;
+    font-family: "Pretendard";
+    cursor: default;
+    z-index: 10;
+`
+
+const SBox = styled.div`
+    width: 500px;
+    height: 300px;
+    margin: auto 0;
+    background-color: #ffffff;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`;
+
+const BoxHeader = styled.div`
+    width: 100%;
+    height: 60px;
+    background-color: #FDF7F5;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 18px;
+`;
+
+const Wrap = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-evenly;
+    align-items: center;
+`
+
+const Button = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 160px;
+    height: 60px;
+    border-radius: 30px;
+    font-size: 20px;
+    background-color: #FFEEE8;
+    cursor: pointer;
+`
+
+const Button1 = styled(Button)`
+    background-color: #FDF7F5;
+`
+
+const Span = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 100%;
+    font-size: 12px;
+    line-height: 20px;
+    word-break: keep-all;
+`
